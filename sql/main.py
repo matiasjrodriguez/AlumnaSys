4e08383480f8257dc67aeb1ba42934eb90b42969
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2024-04-12 14:59:03 -0300

    declaraciones básicas

---
+++ sql/main.py
@@ -0,0 +1,16 @@
+from fastapi import Depends, FastAPI, Path, status
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
