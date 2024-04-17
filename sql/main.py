abe33d33cb59f3ec4e000790ea33c0d903ea4ab1
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2024-04-17 16:59:50 -0300

    typo

--- sql/main.py
+++ sql/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=['*'],
     allow_credentials=False,
     allow_methods=['*'],
     allow_headers=['*']
